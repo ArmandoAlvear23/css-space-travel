cd3ddeff0a2faa4c0476e4e805c5c8ed4a8f0120
Armando Alvear <armando.alvear01@outlook.com>
2022-06-24 21:02:15 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+    //47 https://youtu.be/lRaL-8qZ0mM
+    
+    $output = 
+    '<html>
+        <head>
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+            <link href="https://fonts.googleapis.com/css2?family=Barlow+Condesned:wght@400;700&family=Bellefair&family=Barlow:wght@400;700&display=swap" rel="stylesheet">
+            <link rel="stylesheet" href="index.css">
+        </head>
+        <body class="bg-dark text-white">
+            <div class="container">
+                <h1>Design System</h1>
+                test
+
+                <section id="colors">
+                    <h2><span>01</span> colors</h2>
+
+                    <div>
+                        <div class="bg-dark text-white" style="padding: 3rem 1rem 1rem; border: 1px solid white">
+                            #0B0D17
+                        </div>
+                        <p><span class="accent">RGB 11, 13, 23</span></p>
+                        <p><span class="accent">HSL 230°, 35%, 7%</span></p>
+                    </div>
+                    
+                    <div>
+                        <div class="bg-dark text-white" style="padding: 3rem 1rem 1rem; border: 1px solid white">
+                            #0B0D17
+                        </div>
+                        <p><span class="accent">RGB 11, 13, 23</span></p>
+                        <p><span class="accent">HSL 230°, 35%, 7%</span></p>
+                    </div>
+
+                    <div>
+                        <div class="bg-dark text-white" style="padding: 3rem 1rem 1rem; border: 1px solid white">
+                            #0B0D17
+                        </div>
+                        <p><span class="accent">RGB 11, 13, 23</span></p>
+                        <p><span class="accent">HSL 230°, 35%, 7%</span></p>
+                    </div>  
+                </section>
+
+            </div>
+
+            <script src="index.js"></script>
+        </body>
+    </html> ';
+
+    echo($output);
+?>
